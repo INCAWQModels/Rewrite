cd6549a825661ee47d9d16e72e496da8f38c5834
INCA Water Quality Models <INCA.WQmodels@gmail.com>
2025-03-30 15:52:08 +0200

    more work on tkinter UI

---
+++ inputMockUps.py
@@ -0,0 +1,7 @@
+"""some code to simulate inputs from a parameter set to the tkinter routines"""
+
+landCoverTypes = ["Forest","Wetland","Arable","Pasture","Urban"]
+buckets = ["Direct runoff", "Upper Unsaturated", "Lower Unsaturated", "Groundwater"]
+
+subcatchments = ["SC 1", "SC 2", "SC 3", "SC 4"]
+reaches = ["Reach 1", "Reach 2", "Reach 3", "Reach 4"]
---
+++ testMenu.py
@@ -0,0 +1,152 @@
+"""First steps towards creating an INCA-type UI, could definitely still use some work"""
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+
+from inputMockUps import *
+
+def do_nothing():
+    messagebox.showinfo(message="This feature is not yet implemented.")
+
+def on_closing():
+        if  messagebox.askokcancel(title="Quit",message= "Do you want to quit?"):
+            app.destroy()
+
+class subcatchmentWindow(tk.Toplevel):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.geometry('300x450')
+        self.title('Subcatchment Window')
+        self.makeNotebook()
+        self.protocol("WM_DELETE_WINDOW", on_closing)        
+        
+        ttk.Button(self,
+                text='Close',
+                command=self.destroy).pack(expand=True)
+        
+    def makeNotebook(self):
+        #create a notebook to hold some data to input
+        frame_width=280
+        frame_height=280
+        n=ttk.Notebook(self)
+        n.add(ttk.Frame(n, width=frame_width, height=frame_height), text="Area")
+        n.add(ttk.Frame(n, width=frame_width, height=frame_height), text="Direct runoff")
+        n.add(ttk.Frame(n, width=frame_width, height=frame_height), text="Land use groups")
+        n.add(ttk.Frame(n, width=frame_width, height=frame_height), text="Constants")
+        n.add(ttk.Frame(n, width=frame_width, height=frame_height), text="Deposition")
+        n.pack(fill="both", expand=True)
+
+class reachWindow(tk.Toplevel):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.geometry('300x450')
+        self.title('Reach Window')
+        self.create_menu(reaches)
+
+        ttk.Button(self,
+                text='Close',
+                command=self.destroy).pack(expand=True)
+        
+    def create_menu(self, reaches):
+        self.menubar = tk.Menu(self)
+        chooseReachMenu=tk.Menu(self.menubar,tearoff=0)
+        for reach in reaches:
+            chooseReachMenu.add_command(label=reach, command=do_nothing)
+        chooseReachMenu.add_separator()
+        chooseReachMenu.add_command(label="Exit",command=self.quit)
+        self.menubar.add_cascade(label="Reach",menu=chooseReachMenu)
+        self['menu'] = self.menubar
+
+class landCoverWindow(tk.Toplevel):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.geometry('300x450')
+        self.title('Land Cover Window')
+        
+        self.create_menu(landCoverTypes)
+        self.makeNotebook(buckets)
+
+        ttk.Button(self,
+                text='Close',
+                command=self.destroy).pack(expand=True)
+    
+    
+    def makeNotebook(self,buckets):
+        frame_width=280
+        frame_height=280
+        n=ttk.Notebook(self)
+        n.add(ttk.Frame(n,width=frame_width, height=frame_height),text="General")
+        n.add(ttk.Frame(n,width=frame_width, height=frame_height),text="Snow")
+        for bucket in buckets:
+            n.add(ttk.Frame(n, width=frame_width, height=frame_height),text=bucket )
+        
+        n.pack(fill="both", expand=True)
+
+    def create_menu(self,landCoverTypes):
+        self.menubar = tk.Menu(self)
+        chooseLandCoverMenu=tk.Menu(self.menubar,tearoff=0)
+        for landCoverType in landCoverTypes:
+            chooseLandCoverMenu.add_command(label=landCoverType, command=do_nothing)
+        chooseLandCoverMenu.add_separator()
+        chooseLandCoverMenu.add_command(label="Exit",command=self.quit)
+        self.menubar.add_cascade(label="Land Cover",menu=chooseLandCoverMenu)
+        self['menu'] = self.menubar
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.geometry('400x600')
+        self.title('Main Window')
+        self.protocol("WM_DELETE_WINDOW", on_closing)
+        self.create_menu()
+
+        
+    def create_menu(self):
+        self.menubar = tk.Menu(self)
+        
+        editMenu=tk.Menu(self.menubar,tearoff=0)
+        editMenu.add_command(label="Land Cover",
+                             command=lambda: self.openLandCoverWindow(landCoverTypes))
+        editMenu.add_command(label="Reach",
+                             command=lambda: self.openReachWindow())
+        editMenu.add_command(label="Subcatchment",
+                             command=lambda: self.openSubcatchmentWindow())
+        editMenu.add_separator()
+        editMenu.add_command(label="Exit",command=self.quit)
+        self.menubar.add_cascade(label="Edit",menu=editMenu)
+
+        runMenu=tk.Menu(self.menubar,tearoff=0)
+        runMenu.add_command(label="Calibration", command=do_nothing)
+        runMenu.add_command(label="Scenario",command=do_nothing)
+        self.menubar.add_cascade(label="Run", menu=runMenu)
+
+        manageMenu=tk.Menu(self.menubar,tearoff=0)
+        manageMenu.add_command(label="Parameter Sets", command=do_nothing)
+        manageMenu.add_command(label="Time Series",command=do_nothing)
+
+        self['menu'] = self.menubar
+
+
+    def on_closing(self):
+        if  messagebox.askokcancel("Quit", "Do you want to quit?"):
+            pass
+
+    def openLandCoverWindow(self,landCoverTypes):
+        lc=landCoverWindow(self)
+        lc.attributes("-topmost", 1)
+
+    def openSubcatchmentWindow(self):
+        sc=subcatchmentWindow(self)
+        sc.attributes("-topmost", 1)
+
+    def openReachWindow(self):
+        r=reachWindow(self)
+        r.attributes("-topmost", 1)
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
