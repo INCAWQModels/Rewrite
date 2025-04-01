72faeb1976370f15588fddcc8f81fb2bba9c8723
INCA Water Quality Models <INCA.WQmodels@gmail.com>
2025-04-01 21:48:29 +0200

    Add files via upload

--- code/inputMockUps.py
+++ code/inputMockUps.py
@@ -4,5 +4,7 @@ landCoverTypes = ["Forest","Wetland","Arable","Pasture","Urban"]
 buckets = ["Direct runoff", "Upper Unsaturated", "Lower Unsaturated", "Groundwater"]
 
 subcatchments = ["SC 1", "SC 2", "SC 3", "SC 4"]
-subcatchmentsDictionary = [{"SC 1": 0}, {"SC 2": 1}, {"SC 3": 2}, {"SC 4": 3}]
+subcatchmentTabs=["Area", "Direct Runoff", "Land Use Groups", "Constants", "Deposition"]
+
 reaches = ["Reach 1", "Reach 2", "Reach 3", "Reach 4"]
+reachTabs = ["Dimensions", "Flow", "Parameters"]
